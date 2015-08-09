ee171dcfed5390543e5b817eb45a811160b59bdb
Alex Bock <alexandermbock@gmail.com>
2015-08-09 16:17:03 -0400

    Remove debugging code from main

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,3 @@
-#include "diagnostic.hh"
-#include "buffer.hh"
-#include "location.hh"
-#include "pp.hh"
 #include "options.hh"
 #include "test.hh"
 
@@ -19,11 +15,5 @@ int main(int argc, char** argv) {
         run_tests();
         std::exit(EXIT_SUCCESS);
     }
-
-    buffer b{"file.c", "\tfo\to€bar"};
-    std::cout << "original:\n" << b.data << "---\n---\n";
-    buffer b_pp1 = perform_pp_phase1(b);
-    std::cout << "phase1:\n" << b_pp1.data << "---\n---\n";
-    buffer b_pp2 = perform_pp_phase2(b_pp1);
-    std::cout << "phase2:\n" << b_pp2.data << "---\n---\n";
+    // TODO
 }
